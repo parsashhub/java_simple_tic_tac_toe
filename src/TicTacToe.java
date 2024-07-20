dc5379cea1d09b4e395510be5930637f4f41f87d
Parsa_Shabanpour <parsash1380@gmail.com>
2024-07-20 08:47:43 +0330

    implement the TicTacToe class

---
+++ src/TicTacToe.java
@@ -0,0 +1,87 @@
+public class TicTacToe {
+    private char[][] board;
+    public char currentPlayer;
+
+    // constructor
+    public TicTacToe() {
+        board = new char[3][3];
+        currentPlayer = 'X';
+        init();
+    }
+
+    // init board
+    public void init() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+        // System.out.println(Arrays.deepToString(board));
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean makeValidMove(int row, int col) {
+        if (row >= 0 && col >= 0 && row < 3 && col < 3 && board[row][col] == '-') {
+            board[row][col] = currentPlayer;
+            return true;
+        }
+        return false;
+    }
+
+    public void changePlayer() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+
+    public boolean checkIfWin() {
+        return (checkRows() || checkColumns() || checkDiagonals());
+    }
+
+    private boolean checkRows() {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == currentPlayer &&
+                    board[i][1] == currentPlayer &&
+                    board[i][2] == currentPlayer) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkColumns() {
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] == currentPlayer &&
+                    board[1][i] == currentPlayer &&
+                    board[2][i] == currentPlayer) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkDiagonals() {
+        return ((board[0][0] == currentPlayer &&
+                board[1][1] == currentPlayer &&
+                board[2][2] == currentPlayer) ||
+                (board[0][2] == currentPlayer &&
+                        board[1][1] == currentPlayer &&
+                        board[2][0] == currentPlayer));
+
+    }
+}
